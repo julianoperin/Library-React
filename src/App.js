f8a8eb4091ece9eec00e83f505ec0a4e19a6ef18
‘Juliano <julianoperins@gmail.com>
2020-12-27 16:27:26 -0500

    Added styles to the bookList

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import React from "react";
 import BookContextProvider from "./contexts/BookContext";
 import Navbar from "./components/NavBar";
+import BookList from "./components/BookList";
 
 function App() {
   return (
     <div className="App">
       <BookContextProvider>
         <Navbar />
+        <BookList />
       </BookContextProvider>
     </div>
   );
