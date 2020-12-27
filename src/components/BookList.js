d6728c28cfbd4a72d160c872178b9cb2245f2511
‘Juliano <julianoperins@gmail.com>
2020-12-27 14:36:21 -0500

    Initial commit

---
+++ src/components/BookList.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const BookList = () => {
+  return (
+    <div className="books-list">
+      <ul>
+        <li>aaaaaaaa</li>
+        <li>bbbbbbbbb</li>
+        <li>ccccccccccc</li>
+      </ul>
+    </div>
+  );
+};
+
+export default BookList;
---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <navbar>
+      <h1>TODO APP</h1>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </navbar>
+  );
+};
+
+export default Navbar;
